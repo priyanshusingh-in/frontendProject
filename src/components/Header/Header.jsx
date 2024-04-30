375e36feb73e7c6da8ff4f46365d815fd5d5b9ef
Priyanshu Singh <in.priyanshusingh@gmail.com>
2024-05-01 00:11:46 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h3>Order your second hand items here...</h3>
+                <p>"Give Life Another Chance" - Depicts a colorful clock with the slogan, urging viewers to embrace second-hand opportunities.</p>
+                <a href='#explore-menu'><button>View Items</button></a>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
